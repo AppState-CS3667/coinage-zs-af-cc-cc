6f9a444a104c36fddeaeb73a6cd248070e62e718
z-shaffer <68123195+z-shaffer@users.noreply.github.com>
2021-11-05 18:57:37 -0400

    Factory should work with mint now

--- CoinFactory.java
+++ CoinFactory.java
@@ -1,6 +1,6 @@
 import java.lang.reflect.*;
 /*
- * Coin Factory class to make coin to send to the country's mint
+ * Coin Factory class to makes a coin request to send to the country's mint
  *
  * Author: Zach Shaffer
  * Date: Nov 2021
@@ -16,40 +16,36 @@ public class CoinFactory
 
 	/*
  	* The factory produces a coin
- 	* @param type Takes the type of coin in and creates the base I.E. USDQuarter
- 	* @return cc Returns the final coin to move on to the mint
+ 	* @param Mint takes the country of coin in and sends the request to the proper mint with the value of the coin desired
+ 	* @return cc Returns the finished coin
  	*/	 	
-	public Coin createCoin(String type, double val) 
+	public Coin createCoin(String mint, double val) 
 	{
-		Mint mm = null;
 		Coin cc = null;
 		Class cl = null;
-		try {	cl = Class.forName(type); }
+		try {	cl = Class.forName(mint); }
 		catch (ClassNotFoundException e) 
 		{ 
-			System.err.println("Class not found."); 
+			System.err.println("Mint class not found."); 
 			e.printStackTrace(); 
 		}
 		Object o = null;
-		if (mm == null)
+		try 
+		{ 
+			o = cl.newInstance(); 
+		} 	
+		catch (InstantiationException e) 
 		{
-			try 
-			{ 
-				o = cl.newInstance(); 
-			} 	
-			catch (InstantiationException e) 
-			{
-				System.err.println("Not able to instantiate."); 
-				e.printStackTrace(); 
-			}
-			catch (IllegalAccessException e) 
-			{ 
-				System.err.println("Not able to access."); 
-				e.printStackTrace(); 
-			}
-			cc = (Coin) o.makeCoin(val);			
-		}	
+			System.err.println("Not able to instantiate."); 
+			e.printStackTrace(); 
+		}
+		catch (IllegalAccessException e) 
+		{ 
+			System.err.println("Not able to access."); 
+			e.printStackTrace(); 
+		}
+		cc = (Coin) o.makeCoin(val);
 		return cc;
-	}
+	}	
 }
 	
