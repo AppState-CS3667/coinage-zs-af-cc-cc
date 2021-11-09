864a1f2229917a09285f3fa6cc308cdda3263eba
z-shaffer <68123195+z-shaffer@users.noreply.github.com>
2021-11-09 14:20:36 -0500

    Two methods of creating coins
    Slightly different ways of creating coins we came up with, not sure which is proper, need subteam 2 to review

--- Demo.java
+++ Demo.java
@@ -1,5 +1,12 @@
 public class Demo {
     public static void main(String[] args) {
+        //Two methods of doing it, not sure which one is right.
+        //Method one:
+        Mint m = new USDMint();
+        Coin c = m.makeCoin(.25);
+        System.out.println(c);
+        
+        //Method two:
         CoinFactory fact = new CoinFactory();
         Coin c = fact.createCoin("USDMint",0.25);
         System.out.println(c);
