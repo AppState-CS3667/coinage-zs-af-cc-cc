5ea44adc07cd19f1b2db04e08ef26d6fb59e46ba
z-shaffer <68123195+z-shaffer@users.noreply.github.com>
2021-11-10 10:50:16 -0500

    Now compatible with null coin

--- src/SmeltDemo.java
+++ src/SmeltDemo.java
@@ -7,9 +7,21 @@ public class SmeltDemo {
 		pSmelt.manufacture(penny);
 		
 		for(int index = 0; index < 1000; index++) {
-			if(!i.inspect(penny)) break;
-			if(!i.smooth(penny)) break;
-			if(!i.buff(penny)) break;
+			if(i.inspect(penny) == Coin.Null)
+			{
+				System.out.println(penny);
+				break;
+			}
+			if(i.smooth(penny) == Coin.Null)
+			{
+				System.out.println(penny);
+				break;
+			}
+			if(i.buff(penny) == Coin.Null)
+			{
+				System.out.println(penny);
+				break;
+			}
 		}
 	}
 }
