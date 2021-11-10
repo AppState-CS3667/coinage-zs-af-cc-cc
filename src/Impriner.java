16172b9b37785247f8a10587ce73451ae94c56ab
z-shaffer <68123195+z-shaffer@users.noreply.github.com>
2021-11-10 10:33:30 -0500

    Update Impriner.java

--- src/Impriner.java
+++ src/Impriner.java
@@ -12,36 +12,36 @@ public class Impriner {
 	 * Takes a coin object, and returns true if it passes inspection or false if it fails.
 	 * 		Will fail 1 / 12 times.
 	 */
-	public boolean inspect(Coin c) {
+	public Coin inspect(Coin c) {
 		if(rng.nextInt(12) == 0) {
 			System.out.println("Inspection Failed.");
-			return false;
+			return Coin.NULL;
 		}
 		System.out.println("Inspection Passed.");
-		return true;
+		return c;
 	}
 	/**
 	 * Takes a coin object, and returns true if it passes smoothing or false if it fails.
 	 * 		Will fail 1 / 1000 times.
 	 */
-	public boolean smooth(Coin c) {
+	public Coin smooth(Coin c) {
 		if(rng.nextInt(1000) == 0) {
 			System.out.println("Smoothing Failed.");
-			return false;
+			return Coin.NULL;
 		}
 		System.out.println("Smoothing Passed.");
-		return true;
+		return c;
 	}
 	/**
 	 * Takes a coin object, and returns true if it passes buffing or false if it fails.
 	 * 		Will fail 1 / 1000 times.
 	 */
-	public boolean buff(Coin c) {
+	public Coin buff(Coin c) {
 		if(rng.nextInt(1000) == 0) {
 			System.out.println("Buffing Failed.");
-			return false;
+			return Coin.NULL;
 		}
 		System.out.println("Buffing Passed.");
-		return true;	
+		return c;	
 	}
 }
